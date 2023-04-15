a4f09eb476e991fc75063b21cfa4d566b7aa81f4
JavitoRoGo <javierrodriguezgomez79@gmail.com>
2023-04-15 13:05:24 +0100

    Agregado Project13-Instafilter

---
+++ Project13-Instafilter/Project13-Instafilter/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  Project13-Instafilter
+//
+//  Created by Javier Rodríguez Gómez on 27/1/22.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveComplete), nil)
+    }
+    
+    @objc func saveComplete(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
---
+++ Project13-Instafilter/Project13-Instafilter/Project13_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project13_InstafilterApp.swift
+//  Project13-Instafilter
+//
+//  Created by Javier Rodríguez Gómez on 26/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Project13_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
